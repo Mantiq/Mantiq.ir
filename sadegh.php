ea651fd8cdcb5a0bf1b818e06428c30c47f36701
smpdevsadegh <sadeghtrav@gmail.com>
2016-08-23 16:59:49 +0430

    sadegh 2nd day edition

--- sadegh.php
+++ sadegh.php
@@ -1,7 +1,11 @@
-<<<<<<< HEAD
 <?php get _header?>
 <?php get_footer?>
 =======
 <?php 
 echo "sadegh is .... :|";
->>>>>>> cb25d88f4b36cf4c43794100b22bbff572c7fd14
+
+//sadegh 2nd day
+//1
+//2
+//3
+//4
